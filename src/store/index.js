b57b1f7a733313ba0d2b6411f895784fed3f92b6
irackson <nasirus@protonmail.com>
2021-10-10 00:40:44 -0400

    actually fix non-chrome production bug

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,6 @@ import { rootReducer } from 'reducers';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
